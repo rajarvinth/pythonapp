52170021b9e9116510a80e436edd991e5c81a0eb
rajarvinth <163541064+rajarvinth@users.noreply.github.com>
2024-04-22 16:38:57 +0530

    Update view.py

--- view.py
+++ view.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template
-import os
+from flask import Flask
+
+ 
 
 app = Flask(__name__)
 
+ 
 
 @app.route('/')
-def home():
-    return render_template('index.html')
 
+def index():
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    return '<h1>My Basic Details</h1><p>Name: Arvinthraj T </p> <p>Email: rajarvinth16.com</p> <p>Age: 28</p> <p>District: Salem</p> '
+
+ 
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=5000)
